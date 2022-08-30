5ba9ab4c04bb4a852f7ba982bb84fc0bbe9eb303
Viktor S <bukmopbl4@gmail.com>
2022-08-30 13:33:13 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Modified script for change ID of TeamViewer on MAC
+
+Tested on:
+
+TeamViewer 15.33.7
+MAC OS Intel/M1
+
+Python 3.10.4
+
+No need to Restart your computer
+### Usage:
+```shell
+$ chmod +x TeamViewer-id-changer-M1.py
+$ sudo ./TeamViewer-id-changer-M1.py
+```
---
+++ TeamViewer-id-changer-M1.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import platform
+import random
+import re
+import string
+import sys
+
+print('''
+--------------------------------
+Tested on:
+TeamViewer 15.33.7 ID Changer
+for MAC OS Intel/M1
+Python 3.10.4 
+Version: 0.2 2022
+--------------------------------
+''')
+
+if platform.system() != "Darwin":
+    print("This script can be run only on MAC OS.")
+    sys.exit()
+
+if os.geteuid() != 0:
+    print("This script must be run form root.")
+    sys.exit()
+
+if "SUDO_USER" in os.environ:
+    USERNAME = os.environ["SUDO_USER"]
+    if USERNAME == "root":
+        print("Can not find user name. Run this script via sudo from regular user")
+        sys.exit()
+else:
+    print("Can not find user name. Run this script via sudo from regular user")
+    sys.exit()
+
+HOMEDIRLIB = f"/Users/{USERNAME}/library/preferences/"
+GLOBALLIB = "/library/preferences/"
+
+CONFIGS = []
+
+
+# Find config files
+def listdir_fullpath(d):
+    return [os.path.join(d, f) for f in os.listdir(d)]
+
+for file in listdir_fullpath(HOMEDIRLIB):
+    if 'teamviewer' in file.lower():
+        CONFIGS.append(file)
+
+for file in listdir_fullpath(GLOBALLIB):
+    if 'teamviewer' in file.lower():
+        CONFIGS.append(file)
+
+if not CONFIGS:
+    print('''
+    There is no TemViewer configs found.
+    Maybe you have deleted it manualy or never run TeamViewer after installation.
+    Nothing to delete.
+    ''')
+else:
+    # Delete config files
+    print("Configs found:\n")
+    for file in CONFIGS: print(file)
+    print('''
+This files will be DELETED permanently.
+All TeamViewer settings will be lost
+''')
+    input("Press Enter to continue or CTR+C to abort...")
+
+    for file in CONFIGS:
+        try:
+            os.remove(file)
+        except:
+            print("Cannot delete config files. Permission denied?")
+            sys.exit()
+    print("Done.")
+
+# Find binaryes
+
+TMBINARYES = [
+    '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer',
+    '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer_Service',
+    '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer_Desktop_Proxy',
+    '/Applications/TeamViewer.app/Contents/Helpers/Restarter',
+    '/Applications/TeamViewer.app/Contents/Helpers/TeamViewer_Assignment'
+]
+
+for file in TMBINARYES:
+    if os.path.exists(file):
+        pass
+    else:
+        print(f"File not found: {file}")
+        print("Install TeamViewer correctly")
+        sys.exit()
+
+
+# Patch files
+
+def idpatch(fpath, platf, serial):
+    file = open(fpath, 'r+b')
+    binary = file.read()
+    PlatformPattern = "IOPlatformExpert.{6}"
+    SerialPattern = "IOPlatformSerialNumber%s%s%s"
+
+    binary = re.sub(bytes(PlatformPattern, 'utf-8'), bytes(platf, 'utf-8'), binary)
+    formattedSerialPattern = SerialPattern % ('\x00', "[0-9a-zA-Z]{8,8}", '\x00')
+    formattedSerial = SerialPattern % ('\x00', serial, '\x00')
+    binary = re.sub(bytes(formattedSerialPattern, 'utf-8'), bytes(formattedSerial, 'utf-8'), binary)
+
+    file = open(fpath, 'wb').write(binary)
+    return True
+
+
+def random_generator(size=8, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+RANDOMSERIAL = random_generator(8)
+RANDOMPLATFORM = "IOPlatformExpert" + random_generator(6)
+
+for file in TMBINARYES:
+    try:
+        idpatch(file, RANDOMPLATFORM, RANDOMSERIAL)
+    except Exception as e:
+        print(f"Error: can not patch file {file} [{e}]")
+        sys.exit()
+
+print("PlatformDevice: " + RANDOMPLATFORM)
+print("PlatformSerial: " + RANDOMSERIAL)
+
+try:
+    os.system("sudo killall TeamViewer")
+except:
+    print("Cannot kill TeamViewer")
+    sys.exit()
+print("TeamViewer process was killed.")
+# try:
+#     os.system("sudo rm -rf ~/Desktop/TeamViewer.app")
+#     os.system("sudo cp -R /Applications/TeamViewer.app ~/Desktop/")
+# except:
+#     print("Cannot copy TeamViewer app")
+#     sys.exit()
+# print("TeamViewer was copied.")
+
+# os.system("sudo codesign --force --deep --sign - ~/Desktop/TeamViewer.app")
+# os.system("sudo codesign --force --deep --sign - ~/Desktop/TeamViewer.app/Contents/MacOS/TeamViewer")
+os.system("sudo codesign --force --deep --sign - /Applications/TeamViewer.app")
+os.system("sudo codesign --force --deep --sign - /Applications/TeamViewer.app/Contents/MacOS/TeamViewer")
+
+# try:
+#     os.system("sudo mv  ~/Desktop/TeamViewer.app /Applications/")
+# except:
+#     print("Cannot replaced TeamViewer app")
+#     sys.exit()
+# print("TeamViewer was replaced.")
+
+print('''
+ID changed sucessfully.
+!!!No need to Restart your computer!!!!
+''')
